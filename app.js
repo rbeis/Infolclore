256f356111e9defcf81995a2040652d6801dba49
Breno Reis <breno.s.o.reis60@gmail.com>
2024-09-18 23:32:56 -0300

    Adding tags

--- app.js
+++ app.js
@@ -5,44 +5,48 @@ function pesquisar() {
   // Obtém a seção HTML com o ID "resultados-pesquisa" para inserir os resultados.
   let section = document.getElementById("resultados-pesquisa");
 
-  // Obtém a seção HTML com ID "campo-pesquisa" para 
-  let campoPesquisa = document.getElementById("campo-pesquisa").value
-  campoPesquisa = campoPesquisa.toLowerCase()
+  // Obtém o valor do campo de pesquisa, converte para minúsculas para facilitar a comparação.
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
 
+  // Verifica se o campo de pesquisa está vazio. Se estiver, exibe uma mensagem personalizada e retorna.
   if (!campoPesquisa) {
-    section.innerHTML = "<p>Nada foi encontrado. Procure por alguma lenda ou mito</p>"
-    return resultados
+    section.innerHTML = "<p>Nada foi encontrado. Procure por alguma lenda ou mito</p>";
+    return resultados;
   }
 
+  // Declara variáveis para armazenar o título, descrição e tags em minúsculas para comparação.
   let titulo = "";
   let descricao = "";
+  let tags = "";
 
   // Itera sobre cada elemento (dado) no array "dados".
   for (let dado of dados) {
-    titulo = dado.nome.toLowerCase()
-    descricao = dado.descricao.toLowerCase()
-    //Se titulo includes campoPesquisa então, faça...
-    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
-      // cria um novo elemento
-      resultados += `
-      <div class="item-resultado">
-          <h2>
-              <a href="#">${dado.nome}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Mais Informações</a>     
-      </div>
-      `;
-    }
-    // Concatena HTML para criar um novo elemento "div" para cada resultado, incluindo o nome, descrição e link do dado atual.
+    // Converte o título, descrição e tags do dado atual para minúsculas.
+    tags = dado.tags.toLowerCase();
+    titulo = dado.nome.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
 
-    if (!resultados) {
-      resultados = "<p>Nada foi encontrado</p>"
+    // Verifica se o campo de pesquisa está contido no título, descrição ou tags.
+    // Se estiver, adiciona o resultado à string de resultados.
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Cria um novo elemento "div" para o resultado.
+      resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#">${dado.nome}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais Informações</a>     
+        </div>
+      `;
     }
   }
 
-  // Atribui o HTML gerado para a propriedade innerHTML da seção, substituindo o conteúdo anterior pelos novos resultados.
+  // Verifica se nenhum resultado foi encontrado e exibe uma mensagem caso necessário.
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>";
+  }
+
+  // Atribui os resultados à seção HTML, substituindo o conteúdo anterior.
   section.innerHTML = resultados;
 }
-
-
--- dados.js
+++ dados.js
@@ -3,41 +3,49 @@ let dados = [
         nome: "Curupira",
         descricao: "O Curupira é uma figura lendária do folclore brasileiro, reverenciado como o protetor das matas. Descrito como um ser pequeno e ágil, com cabelos ruivos flamejantes e pés virados para trás, o Curupira habita as florestas mais profundas. Sua aparência peculiar serve como um disfarce, confundindo aqueles que ousam adentrar seu domínio. Com seus assobios misteriosos e pegadas enganosas, o Curupira guia os intrusos para longe, protegendo a natureza e seus habitantes.",
         link: "https://pt.wikipedia.org/wiki/Curupira#:~:text=O%20Curupira%20é%20uma%20figura,%2Dse%20em%20fogo%2Dvivo.",
+        tags: "pe virado floresta mata"
     },
     {
         nome: "Saci-Pererê",
         descricao: "O Saci-Pererê é um menino negro, pequeno e travesso, com apenas uma perna e um gorro vermelho. Ele habita os redemoinhos e gosta de pregar peças nas pessoas, como apagar fogueiras e embaraçar os cavalos. Apesar das travessuras, o Saci é considerado um protetor da natureza.",
         link: "https://pt.wikipedia.org/wiki/Saci-Pererê",
+        tags: "vento perna vermelho"
     },
     {
         nome: "Boitatá",
         descricao: "O Boitatá é uma serpente de fogo que protege as matas e os campos. Com olhos brilhantes e corpo flamejante, ele persegue aqueles que ateiam fogo nas florestas. O Boitatá é um símbolo da importância de preservar a natureza.",
         link: "https://pt.wikipedia.org/wiki/Boitatá",
+        tags: "cobra fogo floresta mata"
     },
     {
         nome: "Mula-sem-Cabeça",
         descricao: "A Mula-sem-Cabeça é uma lenda assustadora que assombra viajantes solitários. Diz-se que ela surge à noite, galopando sem cabeça, com fogo saindo de suas narinas. A lenda conta que ela busca vingança por um passado trágico.",
-        link: "https://www.todamateria.com.br/mula-sem-cabeca/"
+        link: "https://www.todamateria.com.br/mula-sem-cabeca/",
+        tags: "mula cavalo fogo floresta"
     },
     {
         nome: "Iara",
         descricao: "A Iara, também conhecida como Mãe d'Água, é uma sereia de beleza deslumbrante que habita os rios da Amazônia. Ela encanta os homens com sua voz melodiosa e os leva para as profundezas das águas. A Iara é um símbolo da sensualidade e dos mistérios da natureza.",
-        link: "https://pt.wikipedia.org/wiki/Iara"
+        link: "https://pt.wikipedia.org/wiki/Iara",
+        tags: "sereia agua lago rio mar"
     },
     {
         nome: "Lobisomem",
         descricao: "O Lobisomem é uma criatura que se transforma em lobo durante a lua cheia. A lenda diz que ele ataca animais e, às vezes, até mesmo pessoas. O Lobisomem é um símbolo do medo do desconhecido e da natureza selvagem.",
-        link: "https://pt.wikipedia.org/wiki/Lobisomem"
+        link: "https://pt.wikipedia.org/wiki/Lobisomem",
+        tags: "lobo homem"
     },
     {
         nome: "Cuca",
         descricao: "A Cuca é uma figura assustadora que se alimenta de crianças desobedientes. Ela é descrita como uma velha feia, com dentes afiados e unhas longas. A Cuca é usada pelos pais para assustar as crianças e fazê-las obedecer.",
-        link: "https://www.todamateria.com.br/cuca/"
+        link: "https://www.todamateria.com.br/cuca/",
+        tags: "bruxa verde caldeirao feitiço magia"
     },
     {
         nome: "Boto-cor-de-rosa",
         descricao: "O Boto-cor-de-rosa é um boto que, durante a noite, se transforma em um homem bonito para seduzir mulheres e ter filhos. Ele é uma figura popular na região Norte do Brasil, onde é associado à fertilidade e à beleza.",
-        link: "https://www.todamateria.com.br/boto-cor-de-rosa/"
+        link: "https://www.todamateria.com.br/boto-cor-de-rosa/",
+        tags: "rio sedutor lago chapeu"
     }
 ];
 
